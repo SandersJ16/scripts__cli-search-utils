3d606cd165f367968a9c9d466ff7028105b71d49
Justin L Sanders <sandersj1661@gmail.com>
2022-07-23 00:50:11 -0500

    Update tests variable names to be simpler

--- custom_search/tests/test_exact_matcher.py
+++ custom_search/tests/test_exact_matcher.py
@@ -7,7 +7,8 @@ from matchers.exact_matcher import ExactMatcher
 EXACT_MATCHER = ExactMatcher()
 EXACT_MATCHER_CASE_INSENSITIVE = ExactMatcher(case_insensitive=True)
 
-exact_matcher_matches_testcases = {
+
+matches_testcases = {
     "Exact full match":                       ("Test", ["Test"], True),
     "Case-insensitive match":                 ("Test", ["test"], False),
     "non-match":                              ("Test", ["bad"], False),
@@ -17,12 +18,13 @@ exact_matcher_matches_testcases = {
     "Multiple overlapping partial matches":   ("Test", ["es", "st"], True)
 }
 @pytest.mark.parametrize(["name", "terms", "does_match"],
-    exact_matcher_matches_testcases.values(),
-    ids=exact_matcher_matches_testcases.keys())
+    matches_testcases.values(),
+    ids=matches_testcases.keys())
 def test_exact_matcher_matches(name, terms, does_match):
     assert_matches(EXACT_MATCHER, name, terms, does_match)
 
-exact_matcher_indexesOf_testcases = {
+
+indexesOf_testcases = {
     "Full string match":                ("Test", ["Test"], [(0,4)]),
     "No match":                         ("Test", ["test"], []),
     "Parital match at start of string": ("Test", ["Te"], [(0,2)]),
@@ -33,13 +35,13 @@ exact_matcher_indexesOf_testcases = {
     "multiple terms match":             ("TeTeTeTe", ["eTe" , "Te"], [(1,4), (3,6), (5,8), (0,2), (2,4), (4,6), (6,8)])
 }
 @pytest.mark.parametrize(["name", "terms", "expected_indicies"],
-    exact_matcher_indexesOf_testcases.values(),
-    ids=exact_matcher_indexesOf_testcases.keys())
+    indexesOf_testcases.values(),
+    ids=indexesOf_testcases.keys())
 def test_exact_matcher_indexesOf(name, terms, expected_indicies):
     assert_indexesOf(EXACT_MATCHER, name, terms, expected_indicies)
 
 
-exact_matcher_case_insensitive_matches_testcases = {
+matches_case_insensitive_testcases = {
     "Exact full match":                       ("Test", ["Test"], True),
     "Case-insensitive full match":            ("Test", ["test"], True),
     "non-match":                              ("Test", ["bad"], False),
@@ -49,12 +51,13 @@ exact_matcher_case_insensitive_matches_testcases = {
     "Multiple overlapping partial matches":   ("Test", ["Es", "sT"], True)
 }
 @pytest.mark.parametrize(["name", "terms", "does_match"],
-    exact_matcher_case_insensitive_matches_testcases.values(),
-    ids=exact_matcher_case_insensitive_matches_testcases.keys())
+    matches_case_insensitive_testcases.values(),
+    ids=matches_case_insensitive_testcases.keys())
 def test_exact_matcher_case_insensitive_matches(name, terms, does_match):
     assert_matches(EXACT_MATCHER_CASE_INSENSITIVE, name, terms, does_match)
 
-exact_matcher_case_insensitive_indexesOf_testcases = {
+
+indexesOf_case_insensitive_testcases = {
     "Full string match":                ("Test", ["Test"], [(0,4)]),
     "No match":                         ("Test", ["test"], [(0,4)]),
     "Parital match at start of string": ("Test", ["te"], [(0,2)]),
@@ -65,10 +68,11 @@ exact_matcher_case_insensitive_indexesOf_testcases = {
     "multiple terms match":             ("TeTeTeTe", ["etE" , "te"], [(1,4), (3,6), (5,8), (0,2), (2,4), (4,6), (6,8)])
 }
 @pytest.mark.parametrize(["name", "terms", "expected_indicies"],
-    exact_matcher_case_insensitive_indexesOf_testcases.values(),
-    ids=exact_matcher_case_insensitive_indexesOf_testcases.keys())
+    indexesOf_case_insensitive_testcases.values(),
+    ids=indexesOf_case_insensitive_testcases.keys())
 def test_exact_matcher_case_insensitive_indexesOf(name, terms, expected_indicies):
     assert_indexesOf(EXACT_MATCHER_CASE_INSENSITIVE, name, terms, expected_indicies)
+
 
 if __name__ == "__main__":
     exit(pytest.main())
